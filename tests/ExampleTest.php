6ab032c29b225a3a052a2c7b0811e6aeba265b7b
Ashley Webb <ashleyleonawebb@gmail.com>
2015-04-22 17:03:19 -0500

    All tests pass for registering a new user.

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,17 +1,49 @@
 <?php
 
+Use Laracasts\TestDummy\Factory as TestDummy;
+Use Laracasts\Integrated\Services\Laravel\DatabaseTransactions;
+
 class ExampleTest extends TestCase {
 
-	/**
-	 * A basic functional test example.
-	 *
-	 * @return void
-	 */
-	public function testBasicExample()
-	{
-		$response = $this->call('GET', '/');
+  use DatabaseTransactions;
 
-		$this->assertEquals(200, $response->getStatusCode());
-	}
+  /** @test */
+  public function it_displays_homepage()
+  {
+    $this->visit('/')
+         ->see('Laravel 5');
+  }
+
+  /** @test */
+  public function it_registers_a_new_user()
+  {
+    $credentials = ['email' => 'foo@foobar.com'];
+
+    $this->register($credentials)
+         ->verifyInDatabase('users', $credentials)
+         ->andSeePageIs('/home');
+  }
+
+  /** @test */
+  public function it_notifies_a_user_of_registration_errors()
+  {
+
+  }
+
+  protected function register(array $overrides)
+  {
+    $fields = $this->getRegisterFields($overrides);
+
+    return $this->visit('auth/register')
+                ->andSubmitForm('Register', $fields);
+  }
+
+  protected function getRegisterFields(array $overrides)
+  {
+    $user = TestDummy::attributesFor('App\User', $overrides);
+    $user += ['password_confirmation' => $user['password']];
+
+    return $user;
+  }
 
 }
